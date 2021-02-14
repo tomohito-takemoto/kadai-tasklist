fd8652ae4d15bc515eac8715d33cc94d5aa77e1f
tomohito.takemhoto <takemoto82@icloud.com>
2021-02-14 13:36:05 +0000

    add column

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -13,7 +13,7 @@
 
         <div class="container">
             {{-- エラーメッセージ --}}
-            @include('commons.error_messages')
+            @include('commons.error_tasks')
 
             @yield('content')
         </div>
