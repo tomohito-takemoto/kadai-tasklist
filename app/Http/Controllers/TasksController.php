3ee3c8b78dd9365fb1c3b442378745744464c355
tomohito.takemhoto <takemoto82@icloud.com>
2021-02-25 12:01:11 +0000

    やり直し

--- app/Http/Controllers/TasksController.php
+++ app/Http/Controllers/TasksController.php
@@ -79,23 +79,18 @@ class TasksController extends Controller
      */
     public function show($id)
     {
+        if (\Auth::check()) { 
+            
+            // idの値でメッセージを検索して取得	
+            $task = Task::findOrFail($id);
         
-        $data = [];
-        if (\Auth::check()) { // 認証済みの場合
-            // 認証済みユーザを取得
-            $user = \Auth::user();
-            // ユーザの投稿の一覧を作成日時の降順で取得
-            // （後のChapterで他ユーザの投稿も取得するように変更しますが、現時点ではこのユーザの投稿のみ取得します）
-            $tasks = $user->tasks()->orderBy('created_at', 'desc')->paginate(10);
-
-            $data = [
-                'user' => $user,
-                'tasks' => $tasks,
-            ];
+            // メッセージ詳細ビューでそれを表示
+            return view('tasks.show', [
+                'task' => $task,
+            ]);
         }
-
-        // Welcomeビューでそれらを表示
-        return view('welcome', $data);
+        
+        return redirect('/');
     }
 
     /**
