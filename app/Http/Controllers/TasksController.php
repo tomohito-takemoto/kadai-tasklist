ead6f8ac31717ecc82217a3e43b46e078d1fc25c
tomohito.takemhoto <takemoto82@icloud.com>
2021-02-25 13:42:02 +0000

    TasksControllerのshowメソッドの完成

--- app/Http/Controllers/TasksController.php
+++ app/Http/Controllers/TasksController.php
@@ -83,11 +83,15 @@ class TasksController extends Controller
             
             // idの値でメッセージを検索して取得	
             $task = Task::findOrFail($id);
+            
+            // 自分のID == タスクの所有ユーザーのID
+            if(\Auth::id() == $task->user_id){
         
             // メッセージ詳細ビューでそれを表示
             return view('tasks.show', [
                 'task' => $task,
             ]);
+            }
         }
         
         return redirect('/');
